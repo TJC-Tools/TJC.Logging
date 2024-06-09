a16a706e1935575e239d5fb10185da9b906b5637
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-09 13:33:39 -0400

    Create Log Step Extension & Unit Tests

---
+++ TJC.Logging.Tests/Extensions/LogStepTests.cs
@@ -0,0 +1,21 @@
+﻿namespace TJC.Logging.Tests.Extensions;
+
+[TestClass]
+public class LogStepTests
+{
+    private readonly MockTraceLogger _logger = new();
+
+    [TestInitialize]
+    public void Initialize() =>
+        Settings.Settings.ReloadDefaults();
+
+    [TestMethod]
+    public void LogStepTest()
+    {
+        Settings.Settings.Instance.Formatting.ExcludeAll();
+        _logger.LogStep(1);
+        Assert.AreEqual("Step 1", _logger.LastMessage);
+        _logger.LogStep(2);
+        Assert.AreEqual("Step 2", _logger.LastMessage);
+    }
+}
---
+++ TJC.Logging/Extensions/LogStepExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace TJC.Logging.Extensions;
+
+public static class LogStepExtension
+{
+    public static void LogStep(this ILogger logger, int step) =>
+        logger.LogMetadata($"Step {step}",
+                           logLevel: LogLevel.Debug,
+                           frameIndex: 1);
+}
