ea2c5bb7a213537ed86db8f3e25c71d5ece9b8a7
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-09 13:32:38 -0400

    Log Metadata

---
+++ TJC.Logging/Extensions/LogMetadataExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace TJC.Logging.Extensions;
+
+public static class LogMetadataExtension
+{
+    // ReSharper disable ExplicitCallerInfoArgument
+    public static void LogMetadata(this ILogger              logger,
+                                   string                    message    = "",
+                                   LogLevel                  logLevel   = LogLevel.Trace,
+                                   int                       frameIndex = 0,
+                                   [CallerMemberName] string memberName = "",
+                                   [CallerLineNumber] int    lineNumber = 0) =>
+        logger.Log(logLevel: logLevel,
+                   eventId: new EventId(0),
+                   state: new LogState(frameIndex + 1, memberName, lineNumber),
+                   exception: null,
+                   formatter: LogFormatter.Formatter<LogState>(message));
+}
