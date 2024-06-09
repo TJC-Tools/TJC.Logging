eea72340b64192872f0a9692d6e8ae5647c58732
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-09 13:30:45 -0400

    Create Format Settings

---
+++ TJC.Logging/Settings/Settings.cs
@@ -0,0 +1,26 @@
+﻿namespace TJC.Logging.Settings;
+
+public sealed class Settings
+{
+    #region Fields
+
+    private static Lazy<Settings> _instance = new();
+
+    #endregion
+
+    #region Properties
+
+    public static Settings Instance =>
+        _instance.Value;
+
+    public Formatting Formatting { get; set; } = new();
+
+    #endregion
+
+    #region Methods
+
+    public static void ReloadDefaults() =>
+        _instance = new Lazy<Settings>();
+
+    #endregion
+}
