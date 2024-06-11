d47a749c6b1108b65aad90f29f0279a5550dc3dc
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-10 21:43:24 -0400

    Log Get

--- TJC.Logging.Tests/GlobalUsings.cs
+++ TJC.Logging.Tests/GlobalUsings.cs
@@ -1,5 +1,7 @@
 global using Microsoft.Extensions.Logging;
 global using System.Diagnostics;
 global using TJC.Inclusion.Extensions;
+global using TJC.Logging.Enums;
 global using TJC.Logging.Extensions;
+global using TJC.Logging.Extensions.Specialty;
 global using TJC.Logging.Tests.Mocks;
