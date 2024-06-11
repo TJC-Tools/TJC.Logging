1af9b50f9ceeeccc1f3ceb98349739c3b408b2df
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-10 20:39:38 -0400

    Remove Inclusion (Replaced with separate NuGet Package)

--- TJC.Logging/GlobalUsings.cs
+++ TJC.Logging/GlobalUsings.cs
@@ -1,10 +1,9 @@
 global using Microsoft.Extensions.Logging;
 global using System.Diagnostics;
-global using System.Reflection;
 global using System.Runtime.CompilerServices;
+global using TJC.Inclusion.Extensions;
 global using TJC.Logging.Formatter;
 global using TJC.Logging.Helpers;
 global using TJC.Logging.Interfaces;
-global using TJC.Logging.Settings.Components;
 global using TJC.Logging.Settings.Format;
 global using TJC.Logging.States;
