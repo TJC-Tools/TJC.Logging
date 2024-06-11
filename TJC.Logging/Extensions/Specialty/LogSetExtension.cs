920faa15996bb1b07d1c6ad23a6ded3338a8944e
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-10 21:45:41 -0400

    Log Set

--- TJC.Logging/Extensions/Specialty/LogSetExtension.cs
+++ TJC.Logging/Extensions/Specialty/LogSetExtension.cs
@@ -1,12 +1,12 @@
 ﻿namespace TJC.Logging.Extensions.Specialty;
 
-public static class LogGetExtension
+public static class LogSetExtension
 {
     private static FormatGetSetSettings Settings =>
         Logging.Settings.Settings.Instance.Formatting.Specialty.GetSet;
 
     /// <summary>
-    /// Log get of a property or variable.
+    /// Log set of a property or variable.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="logger"></param>
@@ -15,19 +15,20 @@ public static class LogGetExtension
     /// <param name="memberName"></param>
     /// <param name="lineNumber"></param>
     /// <returns></returns>
-    public static T LogGet<T>(this ILogger logger,
-                              T obj,
-                              [CallerArgumentExpression(nameof(obj))]
-                              string argumentName = "",
-                              [CallerMemberName] string memberName = "",
-                              [CallerLineNumber] int lineNumber = -1)
+    public static void LogSet<T>(this ILogger logger,
+                                 ref T obj,
+                                 T value,
+                                 [CallerArgumentExpression(nameof(obj))]
+                                 string argumentName = "",
+                                 [CallerMemberName] string memberName = "",
+                                 [CallerLineNumber] int lineNumber = -1)
     {
         logger.LogMetadata(logLevel: LogLevel.Trace,
-                           specialtyLogType: SpecialtyLogTypes.Get,
-                           message: Settings.ToGetString(obj, argumentName),
+                           specialtyLogType: SpecialtyLogTypes.Set,
+                           message: Settings.ToSetString(obj, value, argumentName),
                            frameIndex: 1,
                            memberName: memberName,
                            lineNumber: lineNumber);
-        return obj;
+        obj = value;
     }
 }
