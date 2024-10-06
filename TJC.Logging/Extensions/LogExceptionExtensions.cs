8484929b2248cbdac9d1d1da408ba0db7c0a8fd2
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-05 21:43:05 -0400

    ✨ feature: Log Exception Extension

---
+++ TJC.Logging/Extensions/LogExceptionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace TJC.Logging.Extensions;
+
+public static class LogExceptionExtensions
+{
+    /// <summary>
+    /// Logs detailed information about the current execution location, including an exception for debugging purposes.
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="exception"></param>
+    /// <param name="logLevel"></param>
+    public static void LogException(this ILogger logger, Exception exception, LogLevel logLevel = LogLevel.Error) =>
+        logger.LogMetadata(exception.ToString(),
+                           logLevel,
+                           frameIndex: 1);
+}
