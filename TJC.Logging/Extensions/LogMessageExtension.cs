98945d095cb36790b1953b6b5eec1eb7b4d73e4c
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-05 21:42:51 -0400

    ✨ feature: Log Message Extension

---
+++ TJC.Logging/Extensions/LogMessageExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace TJC.Logging.Extensions;
+
+public static class LogMessageExtension
+{
+    /// <summary>
+    /// Logs detailed information about the current execution location, including a message for debugging purposes.
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="message"></param>
+    /// <param name="logLevel"></param>
+    public static void LogMessage(this ILogger logger, string message, LogLevel logLevel = LogLevel.Debug) =>
+        logger.LogMetadata(message,
+                           logLevel,
+                           frameIndex: 1);
+}
