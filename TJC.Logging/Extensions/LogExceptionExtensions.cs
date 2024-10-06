17491c0b4715171f4c64e21156ddcc4947dfc890
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-05 22:15:33 -0400

    ✨ feature: Exception Formatting

--- TJC.Logging/Extensions/LogExceptionExtensions.cs
+++ TJC.Logging/Extensions/LogExceptionExtensions.cs
@@ -8,8 +8,28 @@ public static class LogExceptionExtensions
     /// <param name="logger"></param>
     /// <param name="exception"></param>
     /// <param name="logLevel"></param>
-    public static void LogException(this ILogger logger, Exception exception, LogLevel logLevel = LogLevel.Error) =>
-        logger.LogMetadata(exception.ToString(),
+    /// <param name="memberName">Leave blank.</param>
+    /// <param name="lineNumber">Leave blank.</param>
+    public static void LogException123(this ILogger              logger,
+                                       Exception                 exception,
+                                       LogLevel                  logLevel   = LogLevel.Error,
+                                       [CallerMemberName] string memberName = "",
+                                       [CallerLineNumber] int    lineNumber = 0)
+    {
+        var log = new List<string>
+        {
+            $"Type: {exception.GetType().FullName}",
+            $"Message: {exception.Message}",
+            $"Source: {exception.Source}",
+            $"StackTrace: {exception.StackTrace?.Trim()}",
+            $"InnerException: {exception.InnerException}"
+        };
+        var separator = $"{Environment.NewLine}\t";
+        var message   = $"{separator}{string.Join(separator, log)}";
+        logger.LogMetadata(message,
                            logLevel,
-                           frameIndex: 1);
+                           frameIndex: 1,
+                           memberName: memberName,
+                           lineNumber: lineNumber);
+    }
 }
