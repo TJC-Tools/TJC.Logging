41c77f9b8a6decf5a3dcebab990ed276bcc2d866
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-05 21:43:20 -0400

    🚨 tests: Log Message Tests

---
+++ TJC.Logging.Tests/Extensions/LogMessageTests.cs
@@ -0,0 +1,26 @@
+﻿namespace TJC.Logging.Tests.Extensions;
+
+[TestClass]
+public class LogMessageTests
+{
+    private readonly MockTraceLogger _logger = new();
+
+    [TestInitialize]
+    public void Initialize() =>
+        Settings.Settings.ReloadDefaults(); // Reset settings before each test
+
+    [TestMethod]
+    public void LogMessage_ABC()
+    {
+        // Arrange
+        Settings.Settings.Instance.Formatting.ExcludeAll();
+        var message = "ABC";
+
+        // Act
+        _logger.LogMessage(message);
+        var result = _logger.LastMessage;
+
+        // Assert
+        Assert.AreEqual(message, result);
+    }
+}
