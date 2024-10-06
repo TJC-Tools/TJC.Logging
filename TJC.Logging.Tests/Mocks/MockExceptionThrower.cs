d71e635d1b0293b8aef96c045456e8c7fe263f86
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-06 00:13:09 -0400

    🚨 tests: Create Mock Exception Thrower
    To test throwing exceptions multiple layers down

---
+++ TJC.Logging.Tests/Mocks/MockExceptionThrower.cs
@@ -0,0 +1,16 @@
+﻿namespace TJC.Logging.Tests.Mocks;
+
+internal static class MockExceptionThrower
+{
+    public static void ThrowException(string message) =>
+        throw new Exception(message);
+
+    public static void CallMethodToThrowException(string message) =>
+        ThrowException(message);
+
+    public static void ThrowExceptionWithInnerException(string message, string innerMessage) =>
+        throw new Exception(message, new Exception(innerMessage));
+
+    public static void CallMethodToThrowExceptionWithInnerException(string message, string innerMessage) =>
+        ThrowExceptionWithInnerException(message, innerMessage);
+}
