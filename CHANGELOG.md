613ecb9286fb24635f1d1e23651522d22050e5bc
Tyler Carrol <tylercarrol@hotmail.com>
2025-07-19 23:53:07 -0400

    Create composite logger

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Console Logger
+- Composite Logger
 
 ## [0.4.1] - 2024-10-26
 
---
+++ TJC.Logging/Loggers/CompositeLogger.cs
@@ -0,0 +1,25 @@
+﻿namespace TJC.Logging.Loggers;
+
+internal class CompositeLogger(IEnumerable<ILogger> loggers) : ILogger
+{
+    private readonly IEnumerable<ILogger> _loggers = loggers;
+
+    public IDisposable BeginScope<TState>(TState state) where TState : notnull =>
+        NullScope.Instance; // Return a disposable that ends all scopes (if needed)
+
+    public bool IsEnabled(LogLevel logLevel) => _loggers.Any(x => x.IsEnabled(logLevel));
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state,
+                            Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        foreach (var logger in _loggers)
+            if (logger.IsEnabled(logLevel))
+                logger.Log(logLevel, eventId, state, exception, formatter);
+    }
+
+    private class NullScope : IDisposable
+    {
+        public static readonly NullScope Instance = new();
+        public void Dispose() { }
+    }
+}
